ad1a2c6c0d3d54e34be5be3d5b9d1a96813bfd68
Mathieu Tammaro <mathieu.tammaro@gmail.com>
2021-11-08 13:22:49 +0100

    in modules/compute_mrr.py, changed rank j so it starts at 1 and not 0.

--- modules/compute_mrr.py
+++ modules/compute_mrr.py
@@ -4,7 +4,7 @@ def compute_mrr_squad(queries, list_retrived_documents, squad_validation_dataset
         query_index = squad_validation_dataset['question'].index(query)
         relevant_doc = squad_validation_dataset['context'][query_index]
         retrived_documents = list_retrived_documents[i]
-        j = 0
+        j = 1
         while(retrived_documents[j]['dataset'] != 'squad_v2' or retrived_documents[j]['text'] != relevant_doc):
             j += 1
         sum_reci_rank += 1/j
