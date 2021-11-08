ff1e3acaa49242f1dd36d009513a583d904d5675
Mathieu Tammaro <mathieu.tammaro@gmail.com>
2021-11-08 13:24:37 +0100

    in modules/compute_mrr.py, fixed typo on precedent change on the rank j

--- modules/compute_mrr.py
+++ modules/compute_mrr.py
@@ -4,10 +4,10 @@ def compute_mrr_squad(queries, list_retrived_documents, squad_validation_dataset
         query_index = squad_validation_dataset['question'].index(query)
         relevant_doc = squad_validation_dataset['context'][query_index]
         retrived_documents = list_retrived_documents[i]
-        j = 1
+        j = 0
         while(retrived_documents[j]['dataset'] != 'squad_v2' or retrived_documents[j]['text'] != relevant_doc):
             j += 1
-        sum_reci_rank += 1/j
+        sum_reci_rank += 1/(j+1)
     mrr = sum_reci_rank/len(queries)
     return mrr
 
