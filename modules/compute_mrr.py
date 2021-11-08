6cf606934b43b1f814f5e842d9e056b9d19cb126
Mathieu Tammaro <mathieu.tammaro@gmail.com>
2021-11-08 13:20:12 +0100

    In modules/model_evluation.py, fixed variable not referenced at the right place.

--- modules/compute_mrr.py
+++ modules/compute_mrr.py
@@ -1,6 +1,6 @@
 def compute_mrr_squad(queries, list_retrived_documents, squad_validation_dataset):
+    sum_reci_rank = 0
     for i,query in enumerate(queries):
-        sum_reci_rank = 0
         query_index = squad_validation_dataset['question'].index(query)
         relevant_doc = squad_validation_dataset['context'][query_index]
         retrived_documents = list_retrived_documents[i]
