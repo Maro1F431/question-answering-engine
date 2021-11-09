623b432184e62688d44412eff5b67e019afa22c0
Mathieu Tammaro <mathieu.tammaro@gmail.com>
2021-11-09 22:59:48 +0100

    Added evaluation model function in modules/benchmarking.

---
+++ modules/benchmarking/compare_qa_models.py
@@ -0,0 +1,17 @@
+from datasets import load_dataset, load_metric
+from datasets import load_dataset, load_metric
+from transformers import AutoTokenizer, Trainer, AutoModelForQuestionAnswering
+from modules.question_answering.model_evaluation import get_processed_predictions, compute_metrics
+
+def eval_model(model_checkpoint):
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+    model = AutoModelForQuestionAnswering.from_pretrained(model_checkpoint)
+    datasets = load_dataset("squad_v2")
+    validation_dataset = datasets['validation']
+    trainer = Trainer(model)
+    processed_predictions = get_processed_predictions(validation_dataset, trainer, tokenizer)
+    metrics = load_metric("squad_v2")
+    computed_metrics = compute_metrics(processed_predictions, validation_dataset, metrics)
+    print(compute_metrics)
+
+
