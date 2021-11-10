e01375372860175db4f98f5f7b11d2151da7086b
Mathieu Tammaro <mathieu.tammaro@gmail.com>
2021-11-10 09:59:29 +0100

    Removed prints in best_answer.

--- modules/question_answering/best_answer.py
+++ modules/question_answering/best_answer.py
@@ -15,9 +15,6 @@ def pick_best_answer(question, ranked_corpus_ids, embedded_corpus, qa_nlp):
     max_score = 0
     answer = ''
     for id in ranked_corpus_ids:
-        print("id is : ")
-        print(id)
-        print('embedded_corpus size is {}'.format(len(embedded_corpus)))
         context = embedded_corpus[id]['text']
         QA_input = {
             'question': question,
