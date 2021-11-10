96198e080e7f28f9cd6d4438a3acb2665c202090
Mathieu Tammaro <mathieu.tammaro@gmail.com>
2021-11-10 11:35:52 +0100

    Added mrr computing with annoy.

--- modules/indexing/compute_mrr.py
+++ modules/indexing/compute_mrr.py
@@ -1,4 +1,4 @@
-from indexing import batch_indexing
+from indexing import batch_indexing, get_annoy_index, batch_annoy_indexing
 
 def compute_mrr_squad(embedded_corpus, map_question_context, indexing_model, squad_validation_dataset, comparison_metric='dot'):
     '''
@@ -22,5 +22,35 @@ def compute_mrr_squad(embedded_corpus, map_question_context, indexing_model, squ
         ranked_documents_indexes = list_ranked_documents_indexes[i]
         rank = ranked_documents_indexes.index(map_question_context[i]) + 1
         sum_reci_rank += 1/rank
+    mrr = sum_reci_rank/len(queries)
+    return mrr
+
+def compute_mrr_squad_annoy(embedded_corpus, map_question_context, indexing_model, indexing_model_name, top_k, squad_validation_dataset, comparison_metric='dot'):
+    '''
+    computes the MRR of our model with all the queries in the squad_v2 validation set.
+
+            Parameters:
+                    embedded_corpus (list[dict]): Our corpus with the embedding of each entry.
+                    map_question_context (list[int]): Built by the build_corpus function. 
+                    Needed to retrive easily the relevant context of a given query.
+                    indexing_model (obj): Model used to embed the corpus and the queries.
+                    squad_validation_dataset (Dataset (huggingface)): squad_v2 validation set.
+
+
+            Returns:
+                    mrr (float): mrr score on squad_v2.
+    '''
+    queries = squad_validation_dataset['question']
+    annoy_index = get_annoy_index(256, embedded_corpus, 768, indexing_model_name)
+    list_ranked_documents_indexes = batch_annoy_indexing(annoy_index, queries, indexing_model, top_k)
+    sum_reci_rank = 0
+    for i in range(len(queries)):
+        ranked_documents_indexes = list_ranked_documents_indexes[i]
+        try:
+            rank = ranked_documents_indexes.index(map_question_context[i]) + 1
+        except ValueError:
+            rank = 0
+        if rank != 0:
+            sum_reci_rank += 1/rank
     mrr = sum_reci_rank/len(queries)
     return mrr
